fa236241c9123a97e97c005caad2ca6bce89bcd6
mustafaelhdad <Mostafa.eng_2194@eng.kfs.edu.eg>
2024-04-28 03:50:27 +0300

    ...

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,23 +1,24 @@
-import { Link } from 'react-router-dom';
+import React from "react"; // eslint-disable-line no-unused-vars
+import { Link } from "react-router-dom";
 
 const fakeCart = [
   {
     pizzaId: 12,
-    name: 'Mediterranean',
+    name: "Mediterranean",
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: 'Vegetale',
+    name: "Vegetale",
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: 'Spinach and Mushroom',
+    name: "Spinach and Mushroom",
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -25,7 +26,7 @@ const fakeCart = [
 ];
 
 function Cart() {
-  const cart = fakeCart;
+  const cart = fakeCart; // eslint-disable-line no-unused-vars
 
   return (
     <div>
