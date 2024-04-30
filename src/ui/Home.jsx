9511c836289960e8835aabba0e696f28c6bb5382
mustafaelhdad <Mostafa.eng_2194@eng.kfs.edu.eg>
2024-05-01 01:19:30 +0300

    ...

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import CreateUser from "../features/user/CreateUser";
+import { useSelector } from "react-redux";
+import Button from "./Button";
 
 function Home() {
+  const username = useSelector((state) => state.user.username);
+
   return (
     <div className="my-10 px-4 text-center sm:my-16">
       <h1 className="font-semibol mb-8 text-xl md:text-3xl">
@@ -12,7 +16,13 @@ function Home() {
         </span>
       </h1>
 
-      <CreateUser />
+      {username === "" ? (
+        <CreateUser />
+      ) : (
+        <Button to="/menu" type="primary">
+          Continue ordering, {username}
+        </Button>
+      )}
     </div>
   );
 }
