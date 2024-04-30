3749a45c1caf0ccd91fbf71fd67f302f85688462
mustafaelhdad <Mostafa.eng_2194@eng.kfs.edu.eg>
2024-04-30 12:50:44 +0300

    style cart component and cartitem component

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -2,6 +2,7 @@ import React from "react"; // eslint-disable-line no-unused-vars
 import { Link } from "react-router-dom";
 import LinkButton from "../../ui/LinkButton";
 import Button from "../../ui/Button";
+import CartItem from "../cart/CartItem";
 
 const fakeCart = [
   {
@@ -31,16 +32,23 @@ function Cart() {
   const cart = fakeCart; // eslint-disable-line no-unused-vars
 
   return (
-    <div>
+    <div className="px-4 py-3">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2>Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
 
-      <div>
+      <ul className="mb-3 divide-y divide-stone-200 border-b">
+        {cart.map((item) => (
+          <CartItem item={item} key={item.key} />
+        ))}
+      </ul>
+
+      <div className="mt-6 space-x-2">
         <Button to="/order/new" type="primary">
           Order pizzas
         </Button>
-        <button>Clear cart</button>
+
+        <Button type="secondary">Clear cart</Button>
       </div>
     </div>
   );
